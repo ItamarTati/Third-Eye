4a3f197d3ff8d16a723edd1d347f4411bccbd289
Itamar Tati <itamar.tati1994@gmail.com>
2019-02-12 13:28:16 +0000

    Added componets and button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js'
+import Content from './components/Content.js'
+import Events from './components/Events.js'
+import Players from './components/Players.js'
+import Footer from './components/Footer.js'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Header />
+      <Players />
+      <Content />
+      <Events />
+      <Footer />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render()
+    {
+        return (
+            <div>
+                <header>
+                    <button>See Our Rosters</button>
+                </header>
+            </div>
+            
+        );
+    }
+}
