158de21d7f3a89ac4eaacde3dd49aa2b8f5f7251
Itamar Tati <itamar.tati1994@gmail.com>
2019-03-01 12:06:09 +0000

    Added roster

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,21 +5,19 @@ export default class Header extends Component {
     render()
     {
         return (
-            
-                <header>
+            <header>
+                <span>
                 <div className='title'>
-                
                 <video id='video-background' autoPlay loop muted>
-                     <source src={OverwatchVideo} type='video/mp4'/>
+                <source src={OverwatchVideo} type='video/mp4'/>
                 </video>
-                <div className="top">
-                    <h1>Third-Eye Esports</h1>
-                
+                    <div className="top">
+                        <h1>Third-Eye Esports</h1>
+                    </div>
+                    <a href = '#roster'> <div class="arrow"></div> </a> 
                 </div>
-                <a href = '#players'> <div class="arrow"></div> </a> 
-                
-            </div>
-                </header>
+                </span>
+            </header>
             
             
         );
