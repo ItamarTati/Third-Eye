40713e33dd7b53355a3aadae5f61034be9ddb3a3
Itamar Tati <itamar.tati1994@gmail.com>
2019-03-04 17:40:10 +0000

    Webpage model done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header.js'
-import Content from './components/Content.js'
-import Events from './components/Events.js'
-import Players from './components/Players.js'
-import Footer from './components/Footer.js'
+import Media from './components/Media.js'
+import Roster from './components/Roster.js'
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Header />
-      <Players />
-      <Content />
-      <Events />
-      <Footer />
+      <div className="App"> 
+        <Header />
+        <Roster />
+        <Media />
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,19 @@ export default class Header extends Component {
     render()
     {
         return (
+            <div className='title'>
             <header>
                 <span>
-                <div className='title'>
                 <video id='video-background' autoPlay loop muted>
                 <source src={OverwatchVideo} type='video/mp4'/>
                 </video>
                     <div className="top">
                         <h1>Third-Eye Esports</h1>
                     </div>
-                    <a href = '#roster'> <div class="arrow"></div> </a> 
-                </div>
+                    <a href = '#roster'> <div class="arrow"></div> </a>  
                 </span>
             </header>
+            </div>
             
             
         );
