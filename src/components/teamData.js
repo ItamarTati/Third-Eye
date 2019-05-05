a1840d4795037be118f0a1bdf93ba17cf39cd5d3
Itamar Tati <itamar.tati1994@gmail.com>
2019-05-06 00:19:39 +0100

    added more charcaters

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -38,10 +38,13 @@ let teamData =
         "playerName":"Baptiste",
         "imgurl":"/images/Baptiste_portrait.png",
         "playerWiki":`An elite combat medic and ex-Talon operative, Baptiste now uses his skills to help those whose 
-        lives have been impacted by war. He possesses an innate desire to help people. In correspondence obtained by 
-        Trung Le Nguyen, Cuerva noted that Baptiste is a skilled marksman despite being a medic, and boasted unusually 
-        high endurance, versatility, and even luck. Baptiste loves to eat—it's been suggested that his favorite dish is 
-        pork griot and lalo.`
+        lives have been impacted by war. He possesses an innate desire to help people. In correspondence obtained 
+        by Trung Le Nguyen, Cuerva noted that Baptiste is a skilled marksman despite being a medic, and boasted 
+        unusually high endurance, versatility, and even luck. Baptiste loves to eat—it's been suggested that his 
+        favorite dish is pork griot and lalo. Not many people know that he has a sweet tooth, and he loves to kick 
+        back with a tall drink with a lot of ice. He is fluent in Haitian Creole, French, and English. His equipment 
+        is a collection of armor and weaponry acquired over the years. Some are from the black market, and some are 
+        modified from his Caribbean Coalition and Talon gear; Baptiste has always been resourceful. Baptiste is an only child, but has several cousins.`
       },
     {
         "playerName":"D.Va",
@@ -52,8 +55,66 @@ let teamData =
         carefree, irreverant attitude, D.Va is burdened by the emotional cost of defending her homeland, and the facade the 
         South Korean media projects on her. In addition to skills as a mech pilot, D.Va is also a competent mechanic.`
       },
+      {
+        "playerName":"Ana",
+        "imgurl":"/images/Icon-Ana.png",
+        "playerWiki":`One of the founding members of Overwatch, Ana uses her skills and expertise to defend her home 
+        and the people she cares for. Hailing from a long line of decorated soldiers, Ana inspired confidence and 
+        loyalty in her colleagues and endowed her daughter, Fareeha Amari — Pharah, with an intense sense of duty and 
+        honor. Like her daughter, she bore a tattoo of an Eye of Horus as a symbol of protection. Likewise, her callsign 
+        in the Egyptian Military during the Omnic Crisis was "Horus."`
+      },
+      {
+        "playerName":"Lúcio",
+        "imgurl":"/images/Icon-Lúcio.png",
+        "playerWiki":`Lúcio is an international celebrity who inspires social change through his music and actions. 
+        He has a positive, up-beat attitude, and is always looking towards the future. Lúcio wears hard-light skates 
+        that project hard-light "blades," allowing him to traverse any surface.`
+      },
+      {
+        "playerName":"Orisa",
+        "imgurl":"/images/Icon-Orisa.png",
+        "playerWiki":`Orisa is a newly-created omnic with both a heart and personality. Urged to become a hero by 
+        her creator, Efi Oladele, Orisa strives to right wrongs and act as the hero the world needs. While her 
+        relative inexperience can make her an occasional liability, Efi's optimism and willingness to make 
+        modifications to Orisa's adaptive artificial intelligence have always been able to set things right. While 
+        Orisa has much to learn about the world and its functionality, Orisa stands ready to protect both Efi and 
+        Numbani with her growing sense of honor and duty. She is a fan of Lúcio's music. She does not dream in the 
+        manner that humans do.`
+      },
+      {
+        "playerName":"Tracer",
+        "imgurl":"/images/Icon-Tracer.png",
+        "playerWiki":`The former Overwatch agent known as Tracer is a time-jumping adventurer and an irrepressible 
+        force for good. She hails from London, and has traveled all over the world.She was a great admirer of 
+        Tekhartha Mondatta, and was inspired by Mei, an Overwatch scientist. She believes that sometimes, one simply 
+        has to do what is right, even if it's outside the law.`
+      },
+      {
+        "playerName":"Moira",
+        "imgurl":"/images/Icon-Moira.png",
+        "playerWiki":`
+        Equal parts brilliant and controversial, scientist Moira O'Deorain is on the cutting edge of genetic 
+        engineering, searching for a way to rewrite the fundamental building blocks of life. Though O'Deorain will go 
+        to any lengths to make scientific breakthroughs, her work is still unknown to most of the world. But now that 
+        she has been freed from all constraints, it is only a matter of time before everything changes. O'Deorain has a 
+        negative view of Overwatch, claiming that it was responsible for stifling scientific advancement for decades. 
+        She has no interest in global conquest, but is willing to work with those who might have such a goal. 
+        Her intellectual pursuits are driven by her interest in humanity. She is a geneticist first and foremost, and 
+        that's the scope of her ambition. With very flexible morals, O'Deorain prefers to focus on her research and 
+        has no patience for those who urge caution or restraint. Her right hand is heavily scarred; it has been 
+        speculated that it is a result of experimentation or a genetic anomaly.`
+      },
+      {
+        "playerName":"Torbjörn",
+        "imgurl":"/images/Icon-Torbjörn.png",
+        "playerWiki":`Torbjörn Lindholm (called "Torby" by his friends) is one of the greatest weaponsmiths and 
+        engineers in the world. He has a low opinion of omnics, believing them to be incapable of empathy, instead 
+        acting purely on logic, and disagrees with giving them the same rights as humans.`
+      },
   ]
 
 
 }
 export default teamData
+
